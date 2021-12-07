1144ecfbebda659828981493a4bf1a3d837edeb8
AlexLucker <alexandrmasniy@yandex.ru>
2021-12-07 18:01:49 +0500

    Добавьте файлы проекта.

---
+++ MapDetective.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>>
+using namespace std;
+class Map2D;
+class EntityOM; // Создание сущностей с учетом
+
+
+
+class Map2D{ // Должен быть синглом
+public:
+private:
+    vector
+    
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
